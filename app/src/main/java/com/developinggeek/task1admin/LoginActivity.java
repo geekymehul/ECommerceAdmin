cf9a82f96940b6e598e8e978a3d79bebfd5a37b6
geekymehul <mehul.rawat40@gmail.com>
2018-03-01 12:54:49 +0530

    2-way login security

--- app/src/main/java/com/developinggeek/task1admin/LoginActivity.java
+++ app/src/main/java/com/developinggeek/task1admin/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -23,6 +29,8 @@ public class LoginActivity extends AppCompatActivity
     private EditText edtMail ,edtPass;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgress;
+    private DatabaseReference mDatabase;
+    private boolean isValid = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,14 +42,15 @@ public class LoginActivity extends AppCompatActivity
         edtMail = (EditText)findViewById(R.id.login_edt_email);
         edtPass = (EditText)findViewById(R.id.login_edt_pass);
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Admin");
         mProgress = new ProgressDialog(this);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                String mail = edtMail.getText().toString();
-                String pass = edtPass.getText().toString();
+                final String mail = edtMail.getText().toString();
+                final String pass = edtPass.getText().toString();
 
                 if(!TextUtils.isEmpty(mail)||!TextUtils.isEmpty(pass))
                 {
@@ -49,23 +58,57 @@ public class LoginActivity extends AppCompatActivity
                     mProgress.setCanceledOnTouchOutside(false);
                     mProgress.show();
 
-                    mAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful())
+                        public void onDataChange(DataSnapshot dataSnapshot)
+                        {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren())
                             {
-                                mProgress.dismiss();
-                                Toast.makeText(LoginActivity.this, "Could not log you in.. check the email or password", Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                mProgress.dismiss();
-                                Intent loginIntent = new Intent(LoginActivity.this , MainActivity.class);
-                                loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(loginIntent);
+                                Log.i("datasnapshot",snapshot.toString());
+
+                                String mailCheck = snapshot.child("email").getValue().toString();
+                                String passwordCheck = snapshot.child("password").getValue().toString();
+
+                                Log.i("mail and pass", mailCheck+" "+passwordCheck);
+
+                                if(mailCheck.equals(mail) && pass.equals(passwordCheck))
+                                {
+                                    isValid = true;
+                                }
                             }
                         }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
                     });
+                    if(isValid)
+                    {
+
+                        mAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful())
+                                {
+                                    mProgress.dismiss();
+                                    Toast.makeText(LoginActivity.this, "Could not log you in.. check the email or password", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    mProgress.dismiss();
+                                    Intent loginIntent = new Intent(LoginActivity.this , MainActivity.class);
+                                    loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(loginIntent);
+                                }
+                            }
+                        });
+
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Please Use Valid ", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else
                 {
--- app/src/main/java/com/developinggeek/task1admin/MainActivity.java
+++ app/src/main/java/com/developinggeek/task1admin/MainActivity.java
@@ -6,14 +6,18 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -35,14 +39,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private FirebaseAuth mAuth;
     private EditText edtTitle , edtDesc , edtPrice , edtStatus ,edtSubCategory;
-    private Button btnSearch , btnImg ,btnUpload;
-    private ImageButton imgView;
+    private Button btnImg ,btnUpload ,btnImg2 ;
+    private ImageButton imgView , imgView2;
     private DatabaseReference mDatabase;
     private ProgressDialog mProgress;
     private StorageReference mStorage;
-    private Uri mImageUri = null;
+    private Uri mImageUri = null , mImageUri2 = null;
     private String category;
     private Spinner spinner;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -54,12 +59,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         edtDesc = (EditText)findViewById(R.id.main_edt_desc);
         edtPrice = (EditText)findViewById(R.id.main_edt_price);
         edtStatus = (EditText)findViewById(R.id.main_edt_stock);
-        btnSearch = (Button)findViewById(R.id.btn_search);
         btnImg = (Button)findViewById(R.id.main_img_btn);
         imgView = (ImageButton)findViewById(R.id.main_image);
         btnUpload = (Button)findViewById(R.id.main_btn_upload);
         spinner = (Spinner)findViewById(R.id.main_spinner);
         edtSubCategory = (EditText)findViewById(R.id.main_edt_subcategory);
+        btnImg2 = (Button)findViewById(R.id.main_img_btn_2);
+        imgView2 = (ImageButton)findViewById(R.id.main_image2);
+        mToolbar = (Toolbar)findViewById(R.id.main_toolbar);
+
+        setSupportActionBar(mToolbar);
 
         mProgress = new ProgressDialog(this);
         mAuth = FirebaseAuth.getInstance();
@@ -107,6 +116,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 }
             });
 
+            btnImg2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent , 2);
+
+                }
+            });
+
             btnUpload.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view)
@@ -129,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         final String subCategory = edtSubCategory.getText().toString();
 
         if(!TextUtils.isEmpty(title) && !TextUtils.isEmpty(desc) && !TextUtils.isEmpty(price) && !TextUtils.isEmpty(status)
-           && !TextUtils.isEmpty(subCategory) && !TextUtils.isEmpty(category) && mImageUri!=null)
+           && !TextUtils.isEmpty(subCategory) && !TextUtils.isEmpty(category) && mImageUri!=null &&mImageUri2!=null)
         {
 
             mProgress.setTitle("Uploading....");
@@ -137,34 +157,50 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             mProgress.show();
 
             StorageReference filepath = mStorage.child("blog_images").child(mImageUri.getLastPathSegment());
-            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+            {
+                // Uploading First Image
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
                 {
-                    @SuppressWarnings("VisibleForTests") Uri imgUri = taskSnapshot.getDownloadUrl();
+                    @SuppressWarnings("VisibleForTests") final Uri imgUri = taskSnapshot.getDownloadUrl();
 
-                    DatabaseReference productDB = mDatabase.child(category).child(subCategory);
-
-                    HashMap productMap = new HashMap();
-                    productMap.put("title",title);
-                    productMap.put("description",desc);
-                    productMap.put("price",price);
-                    productMap.put("status",status);
-                    productMap.put("category",category);
-                    productMap.put("subcategory",subCategory);
-                    productMap.put("image",imgUri.toString());
-
-                    productDB.setValue(productMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    StorageReference filepath2 = mStorage.child("blog_images").child(mImageUri2.getLastPathSegment());
+                    filepath2.putFile(mImageUri2).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                    {
+                        // Uploading Second Image
                         @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this, "product uploaded", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(MainActivity.this, "error couldnot be uploaded", Toast.LENGTH_SHORT).show();
-                            }
-                        }
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                        {
 
+                            @SuppressWarnings("VisibleForTests") final Uri imgUri2 = taskSnapshot.getDownloadUrl();
+
+                            DatabaseReference productDB = mDatabase.child(category).child(subCategory).child(title);
+
+                            HashMap productMap = new HashMap();
+                            productMap.put("title",title);
+                            productMap.put("description",desc);
+                            productMap.put("price",price);
+                            productMap.put("status",status);
+                            productMap.put("category",category);
+                            productMap.put("subcategory",subCategory);
+                            productMap.put("image",imgUri.toString());
+                            productMap.put("image2",imgUri2.toString());
+                            productDB.setValue(productMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "product uploaded", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "error couldnot be uploaded", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            });
+
+                        }
                     });
+
                 }
 
             });
@@ -178,6 +214,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        switch(id)
+        {
+            case R.id.main_menu_logout : mAuth.signOut();
+                                        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                                        break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
@@ -187,6 +247,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             mImageUri = data.getData();
             imgView.setImageURI(mImageUri);
         }
+
+        if(requestCode==2 && resultCode==RESULT_OK)
+        {
+            mImageUri2 = data.getData();
+            imgView2.setImageURI(mImageUri2);
+        }
+
     }
 
 
